9fb411e7475f68d980cff10d2d4554f3e8c7601e
marko11-w <69992888+marko11-w@users.noreply.github.com>
2025-06-30 01:25:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -136,4 +136,4 @@ def home():
     return "Bot is running.", 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
